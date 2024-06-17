f09c141b73eff985346b8104de5fa79a7cbb26a2
osp-d <edudamirosp@gmail.com>
2024-06-17 14:04:56 +0500

    Add vertical ship positioning

--- src/logic.js
+++ src/logic.js
@@ -43,7 +43,19 @@ class GameBoard {
     this.submarines = [];
   }
 
-  placeShip(shipSize, x, y, orientation = 'h') {
+  #renderShip(array, mode, coordinate, shipClass) {
+    mode === 'h'
+      ? array.forEach((element) => {
+          this.matrix[coordinate][element].push(shipClass);
+          this.matrix[coordinate][element].push(this[shipClass].length - 1);
+        })
+      : array.forEach((element) => {
+          this.matrix[element][coordinate].push(shipClass);
+          this.matrix[element][coordinate].push(this[shipClass].length - 1);
+        });
+  }
+
+  placeShip(shipSize, y, x, orientation = 'h') {
     if (x + shipSize > 10 || y > 10 || x < 1 || y < 1) {
       return 'x- and y-coordinates should be in the range of 1 and 10';
     } else if (shipSize < 1 || shipSize > 4) {
@@ -53,71 +65,101 @@ class GameBoard {
       let positionArray = [];
 
       for (let i = 0; i < shipSize; i++) {
-        canPosition = checkPosition(this.matrix, i, x, y, shipSize);
-
-        console.log(canPosition);
+        canPosition = checkPosition(
+          this.matrix,
+          i,
+          y,
+          x,
+          shipSize,
+          orientation
+        );
 
         if (canPosition === true) {
-          positionArray.push(x + i);
+          orientation === 'h'
+            ? positionArray.push(x + i)
+            : positionArray.push(y + i);
         }
       }
 
       if (canPosition === true) {
         let ship = new Ship(shipSize);
-        let shipClass;
 
         switch (shipSize) {
           case 1:
             if (this.submarines.length < 4) {
               this.submarines.push(ship);
-              shipClass = 'submarines';
+
+              orientation === 'h'
+                ? this.#renderShip(positionArray, 'h', y, 'submarines')
+                : this.#renderShip(positionArray, 'v', x, 'submarines');
             }
             break;
           case 2:
             if (this.destroyers.length < 3) {
               this.destroyers.push(ship);
-              shipClass = 'destroyers';
+
+              orientation === 'h'
+                ? this.#renderShip(positionArray, 'h', y, 'destroyers')
+                : this.#renderShip(positionArray, 'v', x, 'destroyers');
             }
             break;
           case 3:
             if (this.cruisers.length < 2) {
               this.cruisers.push(ship);
-              shipClass = 'cruisers';
+
+              orientation === 'h'
+                ? this.#renderShip(positionArray, 'h', y, 'cruisers')
+                : this.#renderShip(positionArray, 'v', x, 'cruisers');
             }
             break;
           case 4:
             if (this.battleship < 1) {
               this.battleship.push(ship);
-              shipClass = 'battleship';
+              this.#renderShip(positionArray, 'h', y, 'battleship');
+
+              orientation === 'h'
+                ? this.#renderShip(positionArray, 'h', y, 'battleship')
+                : this.#renderShip(positionArray, 'v', x, 'battleship');
             }
             break;
         }
-
-        positionArray.forEach((element) => {
-          this.matrix[y][element].push(shipClass);
-          this.matrix[y][element].push(this[shipClass].length - 1);
-        });
       }
     }
   }
 }
 
-function checkPosition(array, i, x, y, shipSize) {
-  let position = true;
+function checkPosition(matrix, i, y, x, shipSize, orientation) {
+  if (orientation === 'h') {
+    if (
+      matrix[y][x - 1][0] !== undefined ||
+      matrix[y][x + i][0] !== undefined ||
+      matrix[y][x + shipSize][0] !== undefined ||
+      matrix[y - 1][x - 1][0] !== undefined ||
+      matrix[y - 1][x + i][0] !== undefined ||
+      matrix[y - 1][x + shipSize][0] !== undefined ||
+      matrix[y + 1][x + i][0] !== undefined ||
+      matrix[y + 1][x - 1][0] !== undefined ||
+      matrix[y + 1][x + shipSize][0] !== undefined
+    ) {
+      return false;
+    }
 
-  if (
-    array[y][x - 1][0] !== undefined ||
-    array[y][x + i][0] !== undefined ||
-    array[y][x + shipSize][0] !== undefined ||
-    array[y - 1][x - 1][0] !== undefined ||
-    array[y - 1][x + i][0] !== undefined ||
-    array[y - 1][x + shipSize][0] !== undefined ||
-    array[y + 1][x + i][0] !== undefined ||
-    array[y + 1][x - 1][0] !== undefined ||
-    array[y + 1][x + shipSize][0] !== undefined
-  ) {
-    position = false;
+    return true;
+  } else if (orientation === 'v') {
+    if (
+      matrix[y - 1][x][0] !== undefined ||
+      matrix[y + i][x][0] !== undefined ||
+      matrix[y + shipSize][x][0] !== undefined ||
+      matrix[y - 1][x - 1][0] !== undefined ||
+      matrix[y + i][x - 1][0] !== undefined ||
+      matrix[y + shipSize][x - 1][0] !== undefined ||
+      matrix[y + i][x + 1][0] !== undefined ||
+      matrix[y - 1][x + 1][0] !== undefined ||
+      matrix[y + shipSize][x + 1][0] !== undefined
+    ) {
+      return false;
+    }
+
+    return true;
   }
-
-  return position;
 }
--- src/tests.test.js
+++ src/tests.test.js
@@ -22,32 +22,37 @@ test('GameBoard matrix structure', () => {
   expect(firstGameBoard.matrix[0].length).toBe(12);
 });
 
-test('Initial ship placement', () => {
+test('Incorrect input', () => {
   let firstGameBoard = new GameBoard();
 
-  expect(firstGameBoard.placeShip(2, 9, 0)).toBe(
+  expect(firstGameBoard.placeShip(2, 0, 9)).toBe(
     'x- and y-coordinates should be in the range of 1 and 10'
   );
-  expect(firstGameBoard.placeShip(2, 7, -1)).toBe(
+  expect(firstGameBoard.placeShip(2, -1, 7)).toBe(
     'x- and y-coordinates should be in the range of 1 and 10'
   );
-  expect(firstGameBoard.placeShip(2, -1, 0)).toBe(
+  expect(firstGameBoard.placeShip(2, 0, -1)).toBe(
     'x- and y-coordinates should be in the range of 1 and 10'
   );
-  expect(firstGameBoard.placeShip(4, 7, 10)).toBe(
+  expect(firstGameBoard.placeShip(4, 10, 7)).toBe(
     'x- and y-coordinates should be in the range of 1 and 10'
   );
 
   expect(firstGameBoard.placeShip(5, 1, 1)).toBe(
     'Ship size should be in the range of 1 and 4'
   );
+});
+
+test('Initial ship placement', () => {
+  let firstGameBoard = new GameBoard();
+
+  // Horizontal positioning tests
 
   firstGameBoard.placeShip(4, 1, 1);
   expect(firstGameBoard.matrix[1][1][0]).toBe('battleship');
   expect(firstGameBoard.matrix[1][2][0]).toBe('battleship');
   expect(firstGameBoard.matrix[1][3][0]).toBe('battleship');
   expect(firstGameBoard.matrix[1][4][0]).toBe('battleship');
-
   expect(firstGameBoard.matrix[1][1][1]).toBe(0);
   expect(firstGameBoard.matrix[1][2][1]).toBe(0);
   expect(firstGameBoard.matrix[1][3][1]).toBe(0);
@@ -56,7 +61,6 @@ test('Initial ship placement', () => {
   firstGameBoard.placeShip(2, 6, 6);
   expect(firstGameBoard.matrix[6][6][0]).toBe('destroyers');
   expect(firstGameBoard.matrix[6][7][0]).toBe('destroyers');
-
   expect(firstGameBoard.matrix[6][6][1]).toBe(0);
   expect(firstGameBoard.matrix[6][7][1]).toBe(0);
 
@@ -84,4 +88,65 @@ test('Initial ship placement', () => {
   expect(firstGameBoard.matrix[7][7][0]).toBe(undefined);
   firstGameBoard.placeShip(1, 7, 8);
   expect(firstGameBoard.matrix[7][8][0]).toBe(undefined);
+
+  // Vertical positioning tests
+
+  firstGameBoard.placeShip(2, 7, 2, 'v');
+  expect(firstGameBoard.matrix[7][2][0]).toBe('destroyers');
+  expect(firstGameBoard.matrix[8][2][0]).toBe('destroyers');
+  expect(firstGameBoard.matrix[7][2][1]).toBe(1);
+  expect(firstGameBoard.matrix[8][2][1]).toBe(1);
+
+  firstGameBoard.placeShip(2, 5, 1, 'v');
+  expect(firstGameBoard.matrix[5][1][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 5, 2, 'v');
+  expect(firstGameBoard.matrix[5][2][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 5, 3, 'v');
+  expect(firstGameBoard.matrix[5][3][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 7, 1, 'v');
+  expect(firstGameBoard.matrix[7][1][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 7, 3, 'v');
+  expect(firstGameBoard.matrix[7][3][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 9, 1, 'v');
+  expect(firstGameBoard.matrix[9][1][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 9, 2, 'v');
+  expect(firstGameBoard.matrix[9][2][0]).toBe(undefined);
+  firstGameBoard.placeShip(2, 9, 3, 'v');
+  expect(firstGameBoard.matrix[9][3][0]).toBe(undefined);
+});
+
+test('Maximum number of positioned ships', () => {
+  let firstGameBoard = new GameBoard();
+
+  firstGameBoard.placeShip(4, 1, 1);
+  expect(firstGameBoard.matrix[1][1][0]).toBe('battleship');
+  expect(firstGameBoard.matrix[1][2][0]).toBe('battleship');
+  expect(firstGameBoard.matrix[1][3][0]).toBe('battleship');
+  expect(firstGameBoard.matrix[1][4][0]).toBe('battleship');
+
+  firstGameBoard.placeShip(4, 6, 6);
+  expect(firstGameBoard.matrix[6][6][0]).toBe(undefined);
+  expect(firstGameBoard.matrix[6][7][0]).toBe(undefined);
+  expect(firstGameBoard.matrix[6][8][0]).toBe(undefined);
+  expect(firstGameBoard.matrix[6][9][0]).toBe(undefined);
+
+  firstGameBoard.placeShip(1, 3, 1);
+  expect(firstGameBoard.matrix[3][1][0]).toBe('submarines');
+  expect(firstGameBoard.matrix[3][1][1]).toBe(0);
+
+  firstGameBoard.placeShip(1, 3, 3);
+  expect(firstGameBoard.matrix[3][3][0]).toBe('submarines');
+  expect(firstGameBoard.matrix[3][3][1]).toBe(1);
+
+  firstGameBoard.placeShip(1, 3, 5);
+  expect(firstGameBoard.matrix[3][5][0]).toBe('submarines');
+  expect(firstGameBoard.matrix[3][5][1]).toBe(2);
+
+  firstGameBoard.placeShip(1, 3, 7);
+  expect(firstGameBoard.matrix[3][7][0]).toBe('submarines');
+  expect(firstGameBoard.matrix[3][7][1]).toBe(3);
+
+  firstGameBoard.placeShip(1, 3, 9);
+  expect(firstGameBoard.matrix[3][9][0]).toBe(undefined);
+  expect(firstGameBoard.matrix[3][9][1]).toBe(undefined);
 });
